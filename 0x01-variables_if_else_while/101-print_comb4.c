ccedc5c7455f2d8de0bbf3dd2ff732e5f1f4cdab
Rodney Asante <rodneykasante@gmail.com>
2022-12-09 15:48:07 -0800

    Task 11

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -13,7 +13,8 @@ int main(void)
 		j = i / 100;
 		k = (i / 10) % 10;
 		l = i % 10;
-		if (j < k && k < l)													{
+		if (j < k && k < l)
+		{
 			putchar(j + '0');
 			putchar(k + '0');
 			putchar(l + '0');
