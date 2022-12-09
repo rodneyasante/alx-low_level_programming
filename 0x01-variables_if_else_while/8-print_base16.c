b656e3947dcc7f246e9a2a4b29220f807d8c4e6e
Rodney Asante <rodneykasante@gmail.com>
2022-12-09 14:34:46 -0800

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ *  * main - prints hexadecimal
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+	char ch = 0;
+	char ch2 = 'a';
+
+	while (ch < 10)
+	{
+		putchar(ch + '0');
+		ch++;
+	}
+	while (ch2 <= 'f')
+	{
+		putchar (ch2);
+		ch2++;
+	}
+	putchar('\n');
+	return (0);
+}
