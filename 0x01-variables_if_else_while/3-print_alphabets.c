119119fe487808a618e09ace30911939b9582cc7
Rodney Asante <rodneykasante@gmail.com>
2022-12-09 14:00:46 -0800

    Task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,12 +10,12 @@ int main(void)
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
-		putchar(ch);	
-	}	
-	for (ch = 'A'; ch <= 'Z'; ch++)	
-	{		
 		putchar(ch);
-	}	
+	}
+	for (ch = 'A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
