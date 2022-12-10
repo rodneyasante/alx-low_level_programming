71f0bc397428a9a4e27ab7e350fc58d7fe2c417a
Rodney Asante <rodneykasante@gmail.com>
2022-12-09 16:07:05 -0800

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -9,9 +9,9 @@ int main(void)
 	int i, j;
 	int a, b, c, d;
 
-	for (i = 0; i < 100; i++)	
+	for (i = 0; i < 100; i++)
 	{
-		a = i / 10;			
+		a = i / 10;	
 		b = i % 10;
 		for (j = 0; j < 100; j++)
 		{
@@ -24,13 +24,12 @@ int main(void)
 				putchar(32);
 				putchar(c + '0');
 				putchar(d + '0');
-				if (!(a == 9 && b == 8))		
+				if (!(a == 9 && b == 8))
 				{
-					putchar(44);								
+					putchar(44);
 					putchar(32);
 				}
-			
-			}															
+			}
 		}
 	}
 	putchar(10);
