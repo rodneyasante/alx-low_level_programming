952c5fff57742775a62b6637023a9c0f9982d053
Rodney Asante <rodneykasante@gmail.com>
2022-12-13 14:51:09 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *  * print_alphabet - prints alphabets in lower case followed by a new line
+ *   */
+
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+		_putchar(letter);
+		letter++;
+	}
+	_putchar('\n');
+
+}
