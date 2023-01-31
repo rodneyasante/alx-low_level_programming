02ece3388b705aa9a492b35f289a66f036a37151
Rodney Asante <rodneykasante@gmail.com>
2023-01-31 13:41:44 -0800

    Fix Lists.h betty

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,14 +2,14 @@
 #define LISTS_H
 #include <stdio.h>
 #include <stdlib.h>
-/**
- *  * struct listint_s - singly linked list
- *   * @n: integer
- *    * @next: points to the next node
- *     *
- *      * Description: singly linked list node structure
- *       * for alx project
- *        */
+/*
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for alx project
+ */
 typedef struct listint_s
 {
 	 int n;
